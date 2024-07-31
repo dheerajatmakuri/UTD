46c734334fdb5396de69f0b46fb59c8bb1c91906
Dheeraj Atmakuri <dheerajatmakuri10010@gmail.com>
2024-07-31 23:51:54 +0530

    modified:   UT Dallas/src/components/About/About.css modified:   UT Dallas/src/components/Campus/Campus.css modified:   UT Dallas/src/components/Contact/Contact.css modified:   UT Dallas/src/components/Footer/Footer.css modified:   UT Dallas/src/components/Footer/Footer.jsx modified:   UT Dallas/src/components/Hero/Hero.css modified:   UT Dallas/src/components/Programs/Programs.css modified:   UT Dallas/src/components/Testimonials/Testimonials.css

--- UT Dallas/src/components/Footer/Footer.jsx
+++ UT Dallas/src/components/Footer/Footer.jsx
@@ -4,10 +4,10 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className='footer'>
-      <p><b>© The University of Texas at Dallas,</b>  All rights reserved</p>
+      <p><b>© The University of Texas at Dallas</b></p>
       <ul>
-        <li><a href="https://jindal.utdallas.edu/advising/policies/" target="_blank" rel="noopener noreferrer"> Terms of Services</a></li>
-        <li><a href="https://www.utdallas.edu/privacy/" target="_blank" rel="noopener noreferrer">Privacy Policy</a></li>
+        <li><a href="https://jindal.utdallas.edu/advising/policies/" target="_blank" rel="noopener noreferrer"><b>Terms of Services</b></a></li>
+        <li><a href="https://www.utdallas.edu/privacy/" target="_blank" rel="noopener noreferrer"><b>Privacy Policy</b></a></li>
       </ul>
     </div>
   )
